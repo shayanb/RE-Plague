31c4f8ec7325cfd8f1ba792f051911c28d4cf030
Shayan Es <shayanbahal@gmail.com>
2014-12-21 05:20:43 -0500

    initial commit

---
+++ plague_repost.py
@@ -0,0 +1,144 @@
+import requests
+import json
+
+
+
+token = 'TOKEN'
+u_id = 'UID'
+lon = '90.0000'
+lat = '0.0000'
+
+
+headers = { 'Host':'plague.io',
+            'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',
+            'Connection':'keep-alive',
+            'Proxy-Connection':'keep-alive',
+            'Accept':'application/json',
+            'User-Agent':'Plague/1.1.25 (iPhone; iOS 8.1; Scale/2.00)',
+            'Accept-Language':'en',
+            'Accept-Encoding':'gzip, deflate'}
+
+
+
+def vote_repost(post_id):
+    '''
+    Vote up post_id
+    '''
+    url = "http://plague.io/api/votes/repost/"
+    payload = { 'latitude':lat,
+                'longitude':lon,
+                'repost_id':str(post_id),
+                'token':token,
+                'uid':u_id}
+    r = requests.post(url, data=payload, headers=headers)
+    print str(post_id)+ " " + r.text
+
+
+
+def vote_skip(post_id):
+    '''
+    Vote down post_id
+    '''
+    url = "http://plague.io/api/votes/skip/"
+    payload = { 'latitude':lat,
+                'longitude':lon,
+                'repost_id':str(post_id),
+                'token':token,
+                'uid':u_id}
+    r = requests.post(url, data=payload, headers=headers)
+    print str(post_id)+ " " + r.text
+
+
+
+def vote_all():
+    '''
+    Vote all up! plague is contagious
+    '''
+    url = "http://plague.io/api/votes/repost/"
+    for i in xrange (1000,999999999):
+        payload = { 'latitude':lat,
+                    'longitude':lon,
+                    'repost_id':str(i),
+                    'token':token,
+                    'uid':u_id}
+        r = requests.post(url, data=payload, headers=headers)
+        print str(i)+ " " + r.text
+
+
+def send_text(text):
+    url = "http://plague.io/api/posts/"
+    send_text_payload = {
+            'latitude':lat,
+            'longitude':lon,
+            'meta':'{"administrativeArea":"Quebec","country":"Canada","locality":"Montreal"}',
+            'text':text,
+            'token':token,
+            'uid':u_id}
+    r = requests.post(url, data=send_text_payload, headers=headers)
+    print text + " - " + r.text
+
+
+
+def comment(post_id, text):
+    '''
+    Comment on post_id
+    '''
+    url = "http://plague.io/api/posts/"+str(post_id)+"/comments/"
+    comment_payload = {'text':str(text),
+                        'token':token,
+                        'uid':u_id}
+    r = requests.post(url, data=comment_payload, headers=headers)
+    print str(post_id) + text + " - " + r.text
+
+
+
+def photo_post(file,text):
+    '''
+    Upload a picture
+    NOT YET WORKING
+    {u'error': {u'info': u'uid+token required', u'code': u'API_AuthRequired'}}
+    '''
+    url = "http://plague.io/api/upload/"
+    headers_upload = { 'Host':'plague.io',
+                'Content-Type':'multipart/form-data; boundary=Boundary+207E2F629F02A0A1',
+                'Connection':'keep-alive',
+                'Proxy-Connection':'keep-alive',
+                'Accept':'application/json',
+                'User-Agent':'Plague/1.1.25 (iPhone; iOS 8.1; Scale/2.00)',
+                'Accept-Language':'en',
+                'Accept-Encoding':'gzip, deflate'}
+    upfile = {'file': open(file, 'rb')}
+    upload_payload = {'token':token,
+                        'uid':u_id,
+                        'file':upfile}
+    r = requests.post(url, data=upload_payload, headers=headers_upload)
+    print text + " - " + str(r.json())
+
+
+
+
+def post_link(media_link, media_link_preview, text):
+    url = "http://plague.io/api/posts/"
+    post_payload = {
+                    'latitude':lat,
+                    'longitude':lon,
+                    'media':str(media_link),
+                    'media_preview':str(media_link_preview),
+                    'meta':'{"administrativeArea":"Quebec","country":"Canada","locality":"Montreal"}',
+                    'text':text,
+                    'token':token,
+                    'uid':u_id}
+    r = requests.post(url, data=post_payload, headers=headers)
+    print str(media_link) + " - " + text + " - " + r.text
+
+
+
+
+#comment(89957,"Coooool")
+#send_text('Hi From Northpole!')
+#post_link("http://funnypictures.me/wp-content/uploads/2012/12/funny-pictures-new-record-lab-plague.jpg","http://funnypictures.me/wp-content/uploads/2012/12/funny-pictures-new-record-lab-plague.jpg","Plague... Eh?")
+post_link("http://google.com","http://google.com","http://google.com")
+
+
+
+#photo_post("./plague.jpg","I told you, Plague is contagious!")
